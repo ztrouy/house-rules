3eca9d7347d3727cc35d2df92d927fac94a43bb7
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-22 13:44:00 -0500

    Created file and wrote rendering for Home view

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,22 @@
+import { Card, CardBody, CardHeader, CardText } from "reactstrap"
+
+export const Home = ({ loggedInUser }) => {
+    if (!loggedInUser) {
+        return <>Loading</>
+    }
+    
+    return (
+        <div className="">
+            <Card className="w-50 position-absolute top-50 start-50 translate-middle">
+                <CardHeader>
+                    <CardText className="fw-bold fs-2">
+                        Welcome {loggedInUser.firstName}
+                    </CardText>
+                </CardHeader>
+                <CardBody>You should get to your chores!</CardBody>
+            </Card>
+        </div>
+    )
+}
+
+export default Home
