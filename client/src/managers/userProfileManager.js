847c64aad67c36aeb2ef391d2aad2386929ecfe5
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-23 01:25:00 -0500

    Added fetch for getting a single User and their Chore Assignments and Completions

--- client/src/managers/userProfileManager.js
+++ client/src/managers/userProfileManager.js
@@ -3,3 +3,7 @@ const _apiUrl = "/api/userProfile"
 export const getUserProfiles = () => {
     return fetch(_apiUrl).then(res => res.json())
 }
+
+export const getUserProfile = (id) => {
+    return fetch(`${_apiUrl}/${id}`).then(res => res.json())
+}
