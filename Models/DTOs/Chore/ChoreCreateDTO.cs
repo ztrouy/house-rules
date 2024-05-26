b4511cb681aa3c75007ff4165d55a36726dd2333
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-26 18:59:33 -0500

    Added data validation for Name, Difficulty, and ChoreFrequencyDays properties

--- Models/DTOs/Chore/ChoreCreateDTO.cs
+++ Models/DTOs/Chore/ChoreCreateDTO.cs
@@ -5,9 +5,12 @@ namespace HouseRules.Models.DTOs;
 public class ChoreCreateDTO
 {
     [Required]
+    [MaxLength(100, ErrorMessage = "Chore names must be 100 characters or less")]
     public string Name { get; set; }
     [Required]
+    [Range(1,5)]
     public int Difficulty { get; set; }
     [Required]
+    [Range(1, 28)]
     public int ChoreFrequencyDays { get; set; }
 }
